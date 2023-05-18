c4d15005bfda41b19fce62e8c0962e721b7dca76
Lucky Singh <singhlucky007007@gmail.com>
2023-05-18 13:49:44 +0530

    fix redeploy

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ const path = require("path")
 require("./Database/mongooseConnect")
 
 app.use("/",router)
-app.use(express.static(path.join(__dirname,"./frontend/dist")))
+app.use(express.static(path.join(__dirname,"./frontend/dist/assets")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./frontend/dist/index.html"))
+    res.sendFile(path.join(__dirname,"./frontend/dist/assets/index.html"))
 })
 
 app.listen(port,()=>{console.log(` server is running on this port ${port}`)})
