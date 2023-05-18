ab59c6e21e505fb75f217dd602526445de6cc2d3
Lucky Singh <singhlucky007007@gmail.com>
2023-05-18 13:56:55 +0530

    fix all issue

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ const port = process.env.PORT || 4000;
 const app = express();
 const router = require("./Routes/route")
 const path = require("path")
+
 require("./Database/mongooseConnect")
 
 app.use("/",router)
-app.use(express.static(path.join(__dirname,"./frontend/dist")))
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./frontend/dist/index.html"))
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
 })
 
 app.listen(port,()=>{console.log(` server is running on this port ${port}`)})
